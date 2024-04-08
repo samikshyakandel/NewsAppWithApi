f9214f3aea7cce7338fe66dc6a039b42f873b3b5
samikshyakandel <kshasamee8@gmail.com>
2024-04-08 18:25:42 +0545

    Initial Commit

---
+++ lib/Config/MessagesModel.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'package:newsapp_with_api/Config/Colors.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+
+// successMessage(String message) => Fluttertoast.showToast(
+//   msg: message,
+//   toastLength:Toast.LENGTH_SHORT,
+//   gravity: ToastGravity.TOP,
+//   timeInSecForIosWeb: 1,
+//   backgroundColor:greenColor,
+//   textColor: Colors.white,
+//   fontSize:16.0,
+// );
+// errorMessage(String message) => Fluttertoast.showToast(
+//   msg:message,
+//   toastLength: Toast.LENGTH_SHORT,
+//   gravity: ToastGravity.TOP,
+//   timeInSecForIosWeb:1,
+//   backgroundColor: darkPrimaryColor,
+//   textColor:Colors.white,
+//   fontSize: 16.0,
+// );
